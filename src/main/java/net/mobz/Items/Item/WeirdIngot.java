9e30641f1f588e1c877017b948e7def31dc296fd
Globox1997 <mitschke11@web.de>
2020-03-21 16:30:27 -0500

    changed weird settings

--- src/main/java/net/mobz/Items/Item/WeirdIngot.java
+++ src/main/java/net/mobz/Items/Item/WeirdIngot.java
@@ -1,6 +1,7 @@
 package net.mobz.Items.Item;
 
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
@@ -9,6 +10,7 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.world.World;
@@ -30,11 +32,23 @@ public class WeirdIngot extends Item {
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
 
         StatusEffectInstance slow = new StatusEffectInstance(StatusEffect.byRawId(2), 0, 0, false, false);
-        StatusEffectInstance gain = new StatusEffectInstance(StatusEffect.byRawId(22), 0, 0, false, false);
+        StatusEffectInstance gain = new StatusEffectInstance(StatusEffect.byRawId(22), 0, 1, false, false);
         LivingEntity bob = (LivingEntity) entity;
+        Random random = new Random();
+        Random random2 = new Random();
+        Random random3 = new Random();
+        double z1 = (random.nextInt() % 50);
+        double z2 = z1 / 100;
+        double z3 = random2.nextInt() % 50;
+        double z4 = z3 / 100;
+        double z5 = random3.nextInt() % 180;
+        double z6 = z5 / 100;
         if (selected == true) {
             bob.addStatusEffect(slow);
             bob.addStatusEffect(gain);
+            world.addParticle(ParticleTypes.DRAGON_BREATH, bob.getX() + z2, bob.getY() + z6, bob.getZ() + z4, 0.0D,
+                    0.0D, 0.0D);
+                 
         }
 
     }
