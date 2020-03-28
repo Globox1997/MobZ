8aef5082a761ea8465528f93c408fc1f89596337
Globox1997 <mitschke11@web.de>
2020-03-28 09:34:04 -0500

    archer fix

--- src/main/java/net/mobz/Entity/Archer2Entity.java
+++ src/main/java/net/mobz/Entity/Archer2Entity.java
@@ -1,57 +1,30 @@
 package net.mobz.Entity;
 
-import net.mobz.Inits.Configinit;
-import net.mobz.Inits.Soundinit;
-import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.PillagerEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.raid.RaiderEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.CrossbowAttackGoal;
-import net.minecraft.entity.ai.goal.FollowTargetGoal;
-import net.minecraft.entity.ai.goal.LookAtEntityGoal;
-import net.minecraft.entity.ai.goal.RevengeGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.mobz.Inits.Configinit;
+import net.mobz.Inits.Soundinit;
 
 public class Archer2Entity extends PillagerEntity {
 
     public Archer2Entity(EntityType<? extends PillagerEntity> entityType, World world) {
         super(entityType, world);
-        this.experiencePoints = 20;
-    }
-
-    protected void initGoals() {
-        super.initGoals();
-        this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(3, new CrossbowAttackGoal(this, 1.0D, 8.0F));
-        this.goalSelector.add(8, new WanderAroundFarGoal(this, 1.0D));
-        this.goalSelector.add(9, new LookAtEntityGoal(this, PlayerEntity.class, 15.0F, 1.0F));
-        this.goalSelector.add(10, new LookAtEntityGoal(this, MobEntity.class, 15.0F));
-        this.targetSelector.add(1, (new RevengeGoal(this, new Class[] { RaiderEntity.class })).setGroupRevenge());
-        this.targetSelector.add(2, new FollowTargetGoal(this, PlayerEntity.class, true));
-    }
-
-    protected void dropEquipment(DamageSource damageSource_1, int int_1, boolean boolean_1) {
-        return;
-    }
-
-    public void onDeath(DamageSource damageSource_1) {
-        return;
     }
 
     protected void initAttributes() {
         super.initAttributes();
-        this.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(30.0D * Configinit.CONFIGZ.LifeMultiplicatorMob);
-        this.getAttributeInstance(EntityAttributes.ATTACK_DAMAGE).setBaseValue(6.0D * Configinit.CONFIGZ.DamageMultiplicatorMob);
+        this.getAttributeInstance(EntityAttributes.MAX_HEALTH)
+                .setBaseValue(30.0D * Configinit.CONFIGZ.LifeMultiplicatorMob);
+        this.getAttributeInstance(EntityAttributes.ATTACK_DAMAGE)
+                .setBaseValue(6.0D * Configinit.CONFIGZ.DamageMultiplicatorMob);
     }
 
     protected SoundEvent getHurtSound(DamageSource damageSource_1) {
@@ -72,9 +45,13 @@ public class Archer2Entity extends PillagerEntity {
         return viewableWorld_1.intersectsEntities(this) && !viewableWorld_1.containsFluid(this.getBoundingBox())
                 && !viewableWorld_1.isAir(entityPos)
                 && this.world.getLocalDifficulty(entityPos).getGlobalDifficulty() != Difficulty.PEACEFUL
-                && this.world.getLightLevel(lighto) < 9 && this.world.isRaining()
-                && !this.world.isWater(entityPos)
+                && this.world.getLightLevel(lighto) < 9 && this.world.isRaining() && !this.world.isWater(entityPos)
                 && Configinit.CONFIGZ.ArcherSpawn == true;
 
     }
+
+    protected void dropEquipment(DamageSource damageSource_1, int int_1, boolean boolean_1) {
+        return;
+    }
+
 }
