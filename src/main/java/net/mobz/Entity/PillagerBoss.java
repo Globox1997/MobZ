1e4b1dd4ebb22e94049c2bd39a2587e5b02e277c
Globox1997 <mitschke11@web.de>
2020-03-30 09:18:13 -0500

    boss editing

--- src/main/java/net/mobz/Entity/PillagerBoss.java
+++ src/main/java/net/mobz/Entity/PillagerBoss.java
@@ -21,6 +21,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.mob.PillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FireballEntity;
+import net.minecraft.entity.projectile.WitherSkullEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
@@ -32,6 +33,7 @@ import net.minecraft.world.World;
 import net.mobz.Inits.Configinit;
 import net.mobz.Inits.Entityinit;
 import net.minecraft.item.ElytraItem;
+import net.minecraft.entity.mob.GhastEntity;
 public class PillagerBoss extends PillagerEntity {
     private int cooldown = 0;
     private int cooldown2 = 0;
@@ -40,6 +42,15 @@ public class PillagerBoss extends PillagerEntity {
 
     public PillagerBoss(EntityType<PillagerBoss> entityType_1, World world_1) {
         super(entityType_1, world_1);
+    }
+
+    protected void initGoals() {
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 1.0D, true));
+        this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 9.0F));
+        this.targetSelector.add(1, new RevengeGoal(this, new Class[0]));
+        this.targetSelector.add(2, new FollowTargetGoal(this, PlayerEntity.class, false));
+      //  this.targetSelector.add(1, new RevengeGoal(this, new Class[] { SmallZombie.class }).setGroupRevenge());
     }
 
     protected void initEquipment(LocalDifficulty difficulty) {
@@ -55,21 +66,6 @@ public class PillagerBoss extends PillagerEntity {
         this.equipStack(EquipmentSlot.OFFHAND, itemStack2);
      }
 
-    public void onDeath(DamageSource damageSource_1) {
-        return;
-    }
-
-    @Override
-    protected void initGoals() {
-        this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new MeleeAttackGoal(this, 1.0D, true));
-        this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 9.0F));
-        this.targetSelector.add(1, new RevengeGoal(this, new Class[0]));
-        this.targetSelector.add(2, new FollowTargetGoal(this, PlayerEntity.class, false));
-        this.targetSelector.add(1, (new RevengeGoal(this, new Class[] { SmallZombie.class })).setGroupRevenge());
-    }
-
-    @Override
     protected void initAttributes() {
         super.initAttributes();
         this.getAttributeInstance(EntityAttributes.MAX_HEALTH)
@@ -80,18 +76,32 @@ public class PillagerBoss extends PillagerEntity {
                 .setBaseValue(1 * Configinit.CONFIGZ.DamageMultiplicatorMob);
     }
 
+    
+
     @Override
 
     protected void mobTick() {
         World world = this.world;
+        PillagerBoss boss = this;
         StatusEffectInstance slow = new StatusEffectInstance(StatusEffect.byRawId(2), 80, 0, false, false);
         StatusEffectInstance blend = new StatusEffectInstance(StatusEffect.byRawId(15), 20, 0, false, false);
-        if (getTarget() != null && !world.isClient && squaredDistanceTo(getTarget()) < 4000D && canSee(getTarget())) {
+        if (getTarget() != null && !world.isClient && squaredDistanceTo(getTarget()) < 4096D && canSee(getTarget())) {
             cooldown2++;
             cooldown++;
             if (cooldown >= requiredcooldown) {
                 cooldown = 0;
                 attack(getTarget(), 1);
+          /*      LivingEntity target = boss.getTarget();
+                Vec3d vec3d = boss.getRotationVec(1.0F);
+                //    World world = this.world;
+                    double i = target.getX() - (boss.getX() + vec3d.x * 4.0D);
+                    double g = target.getBodyY(0.5D) - (0.5D + boss.getBodyY(0.5D));
+                    double h = target.getZ() - (boss.getZ() + vec3d.z * 4.0D);
+                    WitherSkullEntity fireballEntity = new WitherSkullEntity(world, target, i, g, h);
+                  //  fireballEntity.explosionPower = 0;
+                    fireballEntity.updatePosition(boss.getX() + vec3d.x * 4.0D, boss.getBodyY(0.5D) + 0.5D,
+                            fireballEntity.getZ() + vec3d.z * 4.0D);
+                    world.spawnEntity(fireballEntity);*/
             }
             if (cooldown2 >= 200) {
 
@@ -135,16 +145,34 @@ public class PillagerBoss extends PillagerEntity {
     @Override
     public void attack(LivingEntity target, float f) {
         shower();
-        Vec3d vec3d = this.getRotationVec(1.0F);
-        World world = this.world;
-        double i = target.getX() - (this.getX() + vec3d.x * 4.0D);
-        double g = target.getBodyY(0.5D) - (0.5D + this.getBodyY(0.5D));
-        double h = target.getZ() - (this.getZ() + vec3d.z * 4.0D);
-        FireballEntity fireballEntity = new FireballEntity(world, target, i, g, h);
-        fireballEntity.explosionPower = 0;
-        fireballEntity.updatePosition(this.getX() + vec3d.x * 4.0D, this.getBodyY(0.5D) + 0.5D,
+        Vec3d vec3d_1 = this.getRotationVec(1.0F);
+        double double_3 = target.getX() - (this.getX() + vec3d_1.x * 4.0D);
+        double double_4 = target.getBoundingBox().getYLength() + (double)(target.getHeight() / 2.0F) - (0.5D + this.getY() + (double)(this.getHeight() / 2.0F));
+        double double_5 = target.getZ() - (this.getZ() + vec3d_1.z * 4.0D);
+        WitherSkullEntity fireballEntity_1 = new WitherSkullEntity(world, this, double_3, double_4, double_5);
+       // fireballEntity_1.explosionPower = 1;
+        double double_6 = this.getX() + vec3d_1.x * 4.0D;
+        double double_7 = this.getY() + (double)(this.getHeight() / 2.0F) - 0.5D;
+        double double_8 = this.getZ() + vec3d_1.z * 4.0D;
+        fireballEntity_1.updatePosition(double_6, double_7, double_8);
+        world.spawnEntity(fireballEntity_1);
+      
+       
+
+/*
+        Vec3d vec3d = this.boss.getRotationVec(1.0F);
+    //    World world = this.world;
+        double i = target.getX() - (this.boss.getX() + vec3d.x * 4.0D);
+        double g = target.getBodyY(0.5D) - (0.5D + this.boss.getBodyY(0.5D));
+        double h = target.getZ() - (this.boss.getZ() + vec3d.z * 4.0D);
+        WitherSkullEntity fireballEntity = new WitherSkullEntity(world, target, i, g, h);
+      //  fireballEntity.explosionPower = 0;
+        fireballEntity.updatePosition(this.boss.getX() + vec3d.x * 4.0D, this.boss.getBodyY(0.5D) + 0.5D,
                 fireballEntity.getZ() + vec3d.z * 4.0D);
         world.spawnEntity(fireballEntity);
+      
+
+        
 /*
         BlockPos blockPos = (new BlockPos(PillagerBoss.this)).add(-2 + PillagerBoss.this.random.nextInt(5), 1,
                 -2 + PillagerBoss.this.random.nextInt(5));
--- src/main/java/net/mobz/Renderer/GChickenRenderer.java
+++ src/main/java/net/mobz/Renderer/GChickenRenderer.java
@@ -1,34 +1,3 @@
-/*package net.mobz.Renderer;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
-import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.client.render.entity.model.ChickenEntityModel;
-import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
-
-import net.minecraft.client.render.entity.ChickenEntityRenderer;
-
-@Environment(EnvType.CLIENT)
-public class GChickenRenderer extends MobEntityRenderer<ChickenEntity, ChickenEntityModel<ChickenEntity>> {
-   private static final Identifier SKIN = new Identifier("mobz:textures/entity/gchicken.png");
-
-   public GChickenRenderer(EntityRenderDispatcher entityRenderDispatcher) {
-      super(entityRenderDispatcher, new ChickenEntityModel(), 0.3F);
-   }
-
-   public Identifier getTexture(ChickenEntity chickenEntity) {
-      return SKIN;
-   }
-
-   protected float getCustomAngle(ChickenEntity chickenEntity, float f) {
-      float g = MathHelper.lerp(f, chickenEntity.field_6736, chickenEntity.field_6741);
-      float h = MathHelper.lerp(f, chickenEntity.field_6738, chickenEntity.field_6743);
-      return (MathHelper.sin(g) + 1.0F) * h;
-   }
-}*/
 package net.mobz.Renderer;
 
 import net.fabricmc.api.EnvType;
