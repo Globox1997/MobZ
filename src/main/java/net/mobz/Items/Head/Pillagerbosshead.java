f3b8df44bb247b4746e0eccb0cdd0fc6125696b4
Globox1997 <mitschke11@web.de>
2020-04-01 22:58:50 -0500

    added boss advancement

---
+++ src/main/java/net/mobz/Items/Head/Pillagerbosshead.java
@@ -0,0 +1,10 @@
+package net.mobz.Items.Head;
+
+import net.minecraft.item.Item;
+
+public class Pillagerbosshead extends Item {
+  public Pillagerbosshead(Settings settings) {
+    super(settings);
+  }
+
+}
