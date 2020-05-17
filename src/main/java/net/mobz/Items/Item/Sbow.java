226ace7ce7b71aa11c676aa58a40962e28bc67b0
Globox1997 <mitschke11@web.de>
2020-05-16 23:10:13 -0500

    fixes

--- src/main/java/net/mobz/Items/Item/Sbow.java
+++ src/main/java/net/mobz/Items/Item/Sbow.java
@@ -6,11 +6,23 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
 public class Sbow extends BowItem {
    public Sbow(Settings settings) {
       super(settings);
+      this.addPropertyGetter(new Identifier("pull"), (stack, world, entity) -> {
+         if (entity == null) {
+            return 0.0F;
+         } else {
+            return entity.getActiveItem().getItem() != this ? 0.0F
+                  : (float) (stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / 20.0F;
+         }
+      });
+      this.addPropertyGetter(new Identifier("pulling"), (stack, world, entity) -> {
+         return entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0F : 0.0F;
+      });
    }
 
    @Override
