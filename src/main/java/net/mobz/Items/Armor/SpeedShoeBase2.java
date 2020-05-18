8115f6366fc9ce19b89d9981a5fb783cb980b8e9
Globox1997 <mitschke11@web.de>
2020-05-17 21:00:43 -0500

    double

--- src/main/java/net/mobz/Items/Armor/SpeedShoeBase2.java
+++ src/main/java/net/mobz/Items/Armor/SpeedShoeBase2.java
@@ -12,7 +12,7 @@ import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
 
 public class SpeedShoeBase2 extends ArmorItem {
-    double b = 0.03;
+    double b = 0.03D;
     private static final UUID[] MODIFIERS = new UUID[] { UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"),
             UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"),
             UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"),
