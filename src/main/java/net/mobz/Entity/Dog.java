1c69d5eebe3b132ae6b5489e25762016126ccd2a
Globox1997 <mitschke11@web.de>
2020-05-27 21:26:28 -0500

    nether wolf fix

--- src/main/java/net/mobz/Entity/Dog.java
+++ src/main/java/net/mobz/Entity/Dog.java
@@ -1,18 +1,10 @@
 package net.mobz.Entity;
 
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.goal.LookAroundGoal;
-import net.minecraft.entity.ai.goal.LookAtEntityGoal;
-import net.minecraft.entity.ai.goal.PounceAtTargetGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.WolfEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.BlockPos;
@@ -20,7 +12,6 @@ import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import net.mobz.Config.configz;
-import net.mobz.Entity.Attack.WolfAttack;
 import net.mobz.Inits.Configinit;
 import net.mobz.Inits.Entityinit;
 import net.mobz.Inits.Soundinit;
@@ -30,17 +21,6 @@ public class Dog extends WolfEntity {
     public Dog(EntityType<? extends WolfEntity> entityType, World world) {
         super(entityType, world);
     }
-
-    // @Override
-    // protected void initGoals() {
-    // this.goalSelector.add(1, new SwimGoal(this));
-    // this.goalSelector.add(3, new PounceAtTargetGoal(this, 0.4F));
-    // this.goalSelector.add(2, new WolfAttack(this, 1.0D, false));
-    // this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0D));
-    // this.goalSelector.add(10, new LookAtEntityGoal(this, PlayerEntity.class,
-    // 8.0F));
-    // this.goalSelector.add(10, new LookAroundGoal(this));
-    // }
 
     @Override
     protected void initAttributes() {
